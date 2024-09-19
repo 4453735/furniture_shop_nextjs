eb5157dbd84e5b18c8d267024ad1d81c11d86f21
Artem <4453735@gmail.com>
2024-09-19 20:41:27 +0400

    Added slider to project

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import type { Metadata } from "next";
 import Htag from "@/components/Htag/Htag";
 import Button from "@/components/Button/Button";
+import Slider from "@/components/Slider/Slider";
 
 // export const metadata: Metadata = {
 //   title: "Главная",
@@ -21,12 +22,16 @@ export async function generateMetadata(): Promise<Metadata> {
 export default function Home(): ReactElement {
   return (
     <div className={styles.page}>
-      <Htag tag='h1'>Текст</Htag>
-      <Htag tag='h2'>Тестовый заголовок</Htag>
-      <Htag tag='h2'>Тестовый заголовок 2</Htag>
-      <Button appearance='primary' className='test'>Кнопка</Button>
-      <Button appearance='secondary'>Кнопка</Button>
-      <p>Тест</p>
+      <div className={styles.slider}>
+        <Slider />
+      </div>
+
+      {/*<Htag tag='h1'>Текст</Htag>*/}
+      {/*<Htag tag='h2'>Тестовый заголовок</Htag>*/}
+      {/*<Htag tag='h2'>Тестовый заголовок 2</Htag>*/}
+      {/*<Button appearance='primary' className='test'>Кнопка</Button>*/}
+      {/*<Button appearance='secondary'>Кнопка</Button>*/}
+      {/*<p>Тест</p>*/}
     </div>
   );
 }
